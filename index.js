8b66098440761fd2a57d4ce8fecf23ac46bb9301
abhibhoir222 <abhi.bhoir222@gmail.com>
2023-05-10 13:38:00 +0530

    mangoose main

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-console.log("hello");
-console.log("hello");console.log("hello");
+const mongoose = require('mongoose');
+
+const main = async ()=>{
+    await mongoose.connect("mongodb://localhost:27017/e-comm")
+    const ProductsSchema = new mongoose.Schema({
+        name:String,
+        price:Number,
+        brand:String,
+        category:String
+    });
+    const ProductsModel = mongoose.model('products',ProductsSchema);
+    let data = new ProductsModel({
+        name:"m8",
+        price:500,
+        brand:"Samsung",
+        category:"Mobile"
+    });
+    let result = await data.save();
+    console.log(result);
+
+}
+
+main()
