274eea90916d7fbe6d58a0b45148f096500b4de3
abhibhoir222 <abhi.bhoir222@gmail.com>
2023-05-11 14:18:28 +0530

    fianly push on curd

--- index.js
+++ index.js
@@ -5,35 +5,18 @@ const product = require("./product");
 const app = express();
 app.use(express.json());
 
-
-app.post("/create", async(req, resp) => {
-    let data = new product(req.body);
-    let result = await data.save()
-    console.log(result);
-  resp.send(result);
-});
-
-app.get("/list",async(req,resp)=>{
-    let data = await product.find();
-    resp.send(data);
-})
-
-app.delete("/delete/:_id", async(req,resp)=>{
-    let data = await product.deleteOne(req.params)
-    console.log(req.params);
-    resp.send(data);
-})
-
-app.put("/update/:_id",async(req,resp)=>{
-    console.log(req.params);
-    let data = await product.updateOne(
-        req.params,
+app.get("/search/:key",async(req,resp)=>{
+    console.log(req.params.key);
+    let data = await product.find(
         {
-            $set:req.body
+            "$or":[
+                {"name":{$regex:req.params.key}},
+                {"brand":{$regex:req.params.key}}
+            ]
         }
-
-        )
+    )
     resp.send(data)
 })
+
 
 app.listen(5000);
